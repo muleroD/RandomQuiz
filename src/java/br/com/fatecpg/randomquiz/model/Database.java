5fb0349b18dc1b18b28502205cde8c537e563ee9
J.clebson.s.oliveira@gmail.com <j.clebson.s.oliveira@gmail.com>
2019-04-29 09:45:08 -0300

    Adicionado questões do Quiz

--- src/java/br/com/fatecpg/randomquiz/model/Database.java
+++ src/java/br/com/fatecpg/randomquiz/model/Database.java
@@ -28,10 +28,16 @@ public class Database {
 
     public static ArrayList<Question> getQuiz() {
         ArrayList<Question> quiz = new ArrayList<>();
-        quiz.add(new Question("1+1", "2", new String[]{"3", "1", "2"}));
-        quiz.add(new Question("2+2", "4", new String[]{"4", "2", "1"}));
-        quiz.add(new Question("2+1", "3", new String[]{"1", "3", "4"}));
-
+       quiz.add(new Question("Qual a velocidade da luz no vácuo em m/s ?", "299 792 458", new String[]{"301 458 678", "196 234 129", "396 128 987", "299 792 458" }));
+        quiz.add(new Question("Segundo o Físico Isaac Newton, quais dessas grandezas fundamentais é diretamente proporcional à lei da Inércia ?", "Massa",  new String[]{"Massa", "Comprimento", "Tempo", "Temperatura"}));
+        quiz.add(new Question("Acreditava-se que nada era capaz de fugir das forças de atração de um buraco negro, até que 1974 Stephen Hawking provou que é possível extrair informações de dentro dele por meio de que meio ?", "Radiação", new String[]{"Radiação", "Luz", "Calor", "Gravidade"}));
+        quiz.add(new Question("Força que se apresenta perendicular a superfícei de contato, também conhecida como força de apoio", "Normal", new String[]{"Absoluta", "Normal", "Gravitacional", "Atrito" }));
+        quiz.add(new Question("Semicondutor utilizado como componente eletrônico...", "Diodo", new String[]{"Pastilha eletrolitica", "Piezoelétrico", "Resistência", "Diodo" }));
+        quiz.add(new Question("Elemento químico utilizado para medição da passagem de tempo...", "Carbono", new String[]{"Oxigênio", "Hidrogênio", "Carbono", "Nitrogênio" }));
+        quiz.add(new Question("Lei da física relacionada à elasticidade de um corpo...", "!", new String[]{"Lei de Einsten", "Lei de Pascal", "lei de Hooke", "Lei da Gravidade" }));
+        quiz.add(new Question("Quantos graus de latitude no globo terrestre se faz um fuso horário ?", "15º", new String[]{"15°", "30º", "45º", "60º" }));
+        quiz.add(new Question("Qual a temperatura que precisamos atingir para obtermos o zero absoluto em Celsius ?", "-273", new String[]{"0", "-100", "-178", "-273" }));
+        quiz.add(new Question("Material utilizado por Torricelli para determinar a pressão atmosférica...", "Mercúrio", new String[]{"Alumínio", "Bronze", "Mercúrio", "Prata" }));
         return quiz;
     }
 }
